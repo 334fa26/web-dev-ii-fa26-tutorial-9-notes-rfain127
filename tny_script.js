860b3aef73f4658412d2a06efacc0a9e3e7aebba
rfain127 <rfain127@gmail.com>
2026-09-15 13:36:11 -0500

    Tutorial 9 Notes

---
+++ tny_script.js
@@ -0,0 +1,41 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 8th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Riley Fain
+   Date: 9-15-2026  
+
+*/
+//Execute the function to run and display the countdow clock
+runClock();
+setInterval("runClock()", 1000);
+
+function runClock(){
+//Store the current date and time
+var currentDay = new Date();
+var dateStr = currentDay.toLocaleDateString();
+var timeStr = currentDay.toLocaleTimeString();
+
+//Calculate the days until January 1st
+var newYear = new Date("January 1, 2027");
+var daysLeft = (newYear - currentDay)/(1000*60*60*24);
+
+//Calculate the hours left in the current day
+var hrsLeft = (daysLeft - Math.floor(daysLeft)) *24;
+//Calculate the mins and secs left in current hour
+var minsLeft = (hrsLeft - Math.floor(hrsLeft)) *60;
+var secsLeft = (minsLeft - Math.floor(minsLeft)) *60;
+
+/* Display the current date and time */
+document.getElementById("dateNow").innerHTML = dateStr + "<br />" + timeStr;
+
+//Display the time left until New Years Eve
+document.getElementById("days").textContent = Math.floor(daysLeft);
+document.getElementById("hrs").textContent = Math.floor(hrsLeft);
+document.getElementById("mins").textContent = Math.floor(minsLeft);
+document.getElementById("secs").textContent = Math.floor(secsLeft);
+
+}
